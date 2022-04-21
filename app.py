1383febaaca22c829b26e6abe0789f89e28601ad
Ismail Sahnoun <smil.s@live.fr>
2022-04-21 17:44:33 +0100

    chart fixing

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template,redirect,url_for,request
-import json
-app = Flask(__name__)
 
+app = Flask(__name__)
+@app.route("/")
+def welcome() :
+    data = [
+        ("01-02-2021",3309),
+        ("02-02-2021",2609),
+        ("03-02-2021",2409),
+        ("04-02-2021",6009),
+        ("05-02-2021",7309),
+        ("06-02-2021",8309),
+        ("07-02-2021",6043),
+    ]
+    
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]
+    return render_template("index.html", labels=labels, values=values)
 @app.route("/<coin>")
 def coin(coin) :
     data = [
@@ -16,7 +30,7 @@ def coin(coin) :
     
     labels = [row[0] for row in data]
     values = [row[1] for row in data]
-    return render_template("{}.html".format(coin), labels=json.dumps(labels), values=json.dumps(values))
+    return render_template("{}.html".format(coin), labels=labels, values=values)
 
     
 if __name__== '__main__' :
